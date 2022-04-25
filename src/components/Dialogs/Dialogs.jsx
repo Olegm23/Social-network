e4ca7e6c6b7abb62922fb67c25a0adf2254ec199
Oleg Morkovkin <47777712+Olegm23@users.noreply.github.com>
2022-04-25 14:42:43 +0300

    Add files via upload

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
+import s from '././Dialogs.module.css';
+
+const Dialogs = (props) => {
+  let dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+  let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
+
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        {dialogElements}
+      </div>
+      <div className={s.messages}>
+        {messagesElements}
+      </div>
+    </div>
+  )
+}
+
+export default Dialogs;
